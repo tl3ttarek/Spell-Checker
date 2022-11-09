2a4a949ea8d346ea6587abd031250b8b57ed3a7b
tl3ttarek <tl3ttarek@gmail.com>
2022-11-09 20:26:28 +0200

    Update spell-checker.py

--- spell-checker.py
+++ spell-checker.py
@@ -1,13 +1,39 @@
+import difflib
+
+f = open("dictionary.txt", 'r')
+dic = f.readlines()
+
+def binary_search(alist, word):
+  l, h = 0, len(alist)
+
+  while l <= h:
+    m = (h + l) // 2
+    if word == alist[m]:
+      return True
+    elif alist[m] > word:
+      h = m - 1
+    else:
+      l = m + 1
+
+  return False
+
+def is_correct(word):
+  return binary_search(dic, word)
+
 def spell_checker(word):
   if(not is_correct(word)):
     print(f'Word "{word}" is not right.')
     print(f'Do you mean "{suggest_word(word)}"?')
 
-def is_correct(word):
-  pass
-
 def suggest_word(word):
-  pass
+  ans = dic[0]
+  score = 0
+  for w in dic:
+    f = difflib.SequenceMatcher(None, w, word)
+    if f.ratio() > score:
+      ans = w
+      score = f.ratio()
+  return ans[0:-1]
 
 if __name__ == "__main__":
   text = input("Enter your text: ")
